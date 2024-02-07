0550aa1f64bbdf47543351fb9a6692c82f2cfa70
lgbadluck <lg.badluck@gmail.com>
2024-02-07 22:24:31 +0200

    Added Project 6 JavaScript tabs preview

---
+++ Project-6-JavaScript tabs preview/script.js
@@ -0,0 +1,40 @@
+
+const tabOne    = document.querySelector(".react");
+const tabTwo    = document.querySelector(".vue");
+const tabThree  = document.querySelector(".angular");
+const body      = document.body;
+const item      = document.querySelectorAll(".item");
+const icon      = document.querySelector(".big-icon");
+const active    = document.querySelector(".active");
+
+function setColors(tab, bgColor, iconClass, infoColor) {
+    body.style.backgroundColor = bgColor;
+    icon.innerHTML = `<i class="${iconClass}"></>`;
+    for (let i = 0; i < item.length; i++) {
+        item[i].style.color = infoColor;
+    }
+}
+
+tabOne.addEventListener("click", ()=>{
+    setColors(tabOne, "#00d8ff", "fab fa-react", "#00d8ff");
+    tabOne.classList.add("tabOne");
+    tabTwo.classList.remove("tabOne");
+    tabThree.classList.remove("tabOne");
+});
+
+tabTwo.addEventListener("click", ()=>{
+    setColors(tabTwo, "#42b883", "fab fa-vuejs", "#42b883");
+    tabOne.classList.remove("tabOne");
+    tabTwo.classList.add("tabOne");
+    tabThree.classList.remove("tabOne");
+});
+
+tabThree.addEventListener("click", ()=>{
+    setColors(tabThree, "#b52e31", "fab fa-angular", "#b52e31");
+    tabOne.classList.remove("tabOne");
+    tabTwo.classList.remove("tabOne");
+    tabThree.classList.add("tabOne");
+});
+
+setColors(tabOne, "#00d8ff", "fab fa-react", "#00d8ff");
+tabOne.classList.add("tabOne");
